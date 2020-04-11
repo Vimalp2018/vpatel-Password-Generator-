89f2b33fbe409c76e90e17beafcf6e10e6aa0974
Your Name <vimalpatel@vimals-mbp.home>
2020-04-11 15:19:17 -0400

    second

--- script.js
+++ script.js
@@ -8,8 +8,57 @@ function writePassword() {
 
   passwordText.value = password;
 
-  
+}
 
+function generatePassword() {
+
+ var Passwordlength = parseInt(prompt("What length would you like for your password?"));
+        if (Passwordlength < 8 || Passwordlength > 128)
+         {
+        alert("Please try again password must be more than 8 characters and less than 128 characters");
+        }
+        else 
+        {
+         var lowercaseAZ = confirm("Do you want lowercase letters in your password?");
+         var uppercaseAZ = confirm("Do you want uppercase letters in your password?");
+         var numbers = confirm("Do you want numbers in your password?")
+         var specialC = confirm("Do you want special characters in your password?")
+        }
+
+        if (lowercaseAZ == false && uppercaseAZ == false && numbers == false && specialC == false) {
+        alert("sorry try again at lease one character must be selected");
+        }
+
+        if (lowercaseAZ == true) {
+        characters.push(lowercaseAZcharacters)
+        }
+
+        if (uppercaseAZ == true) {
+        characters.push(uppercaseAZcharacters)
+        }
+
+        if (numbers == true) {
+        characters.push(numberscharacters)
+        }
+        if (specialC == true) {
+        characters.push(specialCcharacters)
+        }
+
+var characters = [];
+var lowercaseAZcharacters = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
+var uppercaseAZcharacters = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","W","X","Y","Z");
+var numberscharacters = ("1","2","3","4","5","6","7","8","9","0")
+var specialCcharacters = ("!","@","#","$","%","^","&","*","(",")","{","}","|","[","]",";","'",":","<",">","?","/");
+
+var passwordText = ""  
+
+    for (var i = 0; i < Passwordlength.length; i++) {
+
+      passwordText = passwordText + characters[Math.floor(Math.random() * Passwordlength.length) ]
+
+}
+      
+return passwordText;
 
 
 
@@ -18,6 +67,14 @@ function writePassword() {
 
 
 }
+
+
+
+
+
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
